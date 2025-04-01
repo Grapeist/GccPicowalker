8453420773dee875235215c1359e2a427a824ab2
mamba2410 <callummcgregor54@gmail.com>
2025-04-01 22:24:12 +0100

    usb: don't sleep when plugged in

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 #include "drivers/eeprom_pico_m95512.h"
 #include "picowalker.h"
 #include "picowalker-defs.h"
+#include "drivers/power_pico.h"
 
 extern void (*current_loop)(void);
 int main() {
@@ -104,10 +105,18 @@ int main() {
 
 void tud_mount_cb(void) {
     printf("[Info] tusb mounted\n");
+
+    // Halt sleep timer so we don't mess up tusb
+    power_sleep_enabled = false;
+
 }
 
 void tud_umount_cb(void) {
     printf("[Info] tusb unmounted\n");
+
+    // Unmount and now we're safe to sleep
+    power_sleep_enabled = true;
+    set_user_idle_timer();
 }
 
 void tud_suspend_cb(bool remote_wakeup_en) { (void)remote_wakeup_en; }
