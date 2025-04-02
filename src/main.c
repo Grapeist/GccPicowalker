ccee22912f263068db60368d1f188874ce902e72
mamba2410 <callummcgregor54@gmail.com>
2025-04-02 21:49:34 +0100

    usb: can now read eeprom from usb

--- src/main.c
+++ src/main.c
@@ -140,6 +140,7 @@ void cdc_task(void) {
       //    $ dd if=/dev/zero of=/dev/ttyACM0 count=10000
       tud_cdc_write(buf, count);
       tud_cdc_write_flush();
+      printf(buf);
     }
   }
 }
